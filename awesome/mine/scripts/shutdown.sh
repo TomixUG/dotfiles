50429fcf62cae4b00f21ae03656715fd6195f3d2
TomixUG <tomas.sousek1@gmail.com>
2023-02-03 22:14:47 +0100

    awesome: add turn off screen to shutdown menu

--- awesome/mine/scripts/shutdown.sh
+++ awesome/mine/scripts/shutdown.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-selection=$(echo -e "shutdown\nlogout\nreboot\nlock\nsuspend\nreload wm" | rofi -dmenu -auto-select -theme ~/.config/rofi/shutdown.rasi)
+selection=$(echo -e "shutdown\nlogout\nreboot\nlock\nsuspend\nreload wm\nturn off screen" | rofi -dmenu -auto-select -theme ~/.config/rofi/shutdown.rasi)
 
 case $selection in
 	shutdown) shutdown now ;;
@@ -9,5 +9,6 @@ case $selection in
 	lock) slock ;;
 	suspend) systemctl suspend ;;
 	"reload wm") echo 'awesome.restart()' | awesome-client ;;
+	"turn off screen") xset dpms force off ;;
 	*) ;;
 esac
