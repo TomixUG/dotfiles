057e756e83913dea0b926587e59fedf1ca659d0b
TomixUG <tomas.sousek1@gmail.com>
2023-02-04 02:57:37 +0100

    add rofi assist theme

--- awesome/mine/scripts/assist/instantassist.sh
+++ awesome/mine/scripts/assist/instantassist.sh
@@ -2,12 +2,12 @@
 
 LOCATION="/home/$USER/.dothub/awesome/mine/scripts/assist"
 
-ASSIST=$(ls $LOCATION/assists | sed 's/.sh//' | rofi -dmenu -auto-select -theme ~/.config/rofi/runner.rasi | grep -o '^.')
+ASSIST=$(ls $LOCATION/assists | sed 's/.sh//' | rofi -dmenu -auto-select -theme ~/.config/rofi/assist.rasi | grep -o '^.')
 
 [ -z "$ASSIST" ] && exit # if nothing selected
 
 if [ -d $LOCATION/assists/"$ASSIST" ]; then
-    ASSIST2="$(ls $LOCATION/assists/$ASSIST | sed 's/.sh//' | rofi -dmenu -auto-select -theme ~/.config/rofi/runner.rasi | grep -o '^.')"
+    ASSIST2="$(ls $LOCATION/assists/$ASSIST | sed 's/.sh//' | rofi -dmenu -auto-select -theme ~/.config/rofi/assist.rasi | grep -o '^.')"
     [ -z "$ASSIST2" ] && exit
     SCRIPTPATH=$LOCATION/assists/"$ASSIST/$ASSIST2"
 else
