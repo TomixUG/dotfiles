140c2f8f4ccc5952d2802326a29caed2a21d84a3
TomixUG <tomas.sousek1@gmail.com>
2023-04-01 16:52:57 +0200

    add polkit

--- xmonad/mine/scripts/autostart.sh
+++ xmonad/mine/scripts/autostart.sh
@@ -2,6 +2,9 @@
 xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1-0 --mode 1280x1024 --pos 1920x56 --rotate normal 
 sleep 1
 
+# fix mouse cursor
+xsetroot -cursor_name left_ptr
+
 # polybar
 # ~/.config/polybar/launch.sh
 
@@ -29,3 +32,6 @@ discord &
 sleep 2
 nitrogen --restore &
 telegram-desktop -startintray &
+
+# polkit
+exec /usr/lib/mate-polkit/polkit-mate-authentication-agent-1
