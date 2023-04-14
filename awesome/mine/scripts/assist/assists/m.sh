c919eee3f9d28093dc5922ba84f6739efe3592d0
TomixUG <tomas.sousek1@gmail.com>
2023-04-14 23:43:35 +0200

    change play pause assist to play/pause spotify

--- awesome/mine/scripts/assist/assists/m.sh
+++ awesome/mine/scripts/assist/assists/m.sh
@@ -2,19 +2,20 @@
 
 # assist: play/pause music
 
-if ! command -v spotify >/dev/null 2>&1; then
-    playerctl play-pause
-    exit 0
-fi
-
-if pgrep spotify >/dev/null 2>&1; then
-    # pauses spotify player
-    spoticli t
-    exit
-else
-    if playerctl status || true | grep .; then
-        playerctl play-pause
-    else
-        spoticli
-    fi
-fi
+# if ! command -v spotify >/dev/null 2>&1; then
+# playerctl play-pause
+dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
+    # exit 0
+# fi
+#
+# if pgrep spotify >/dev/null 2>&1; then
+#     # pauses spotify player
+#     spoticli t
+#     exit
+# else
+#     if playerctl status || true | grep .; then
+#         playerctl play-pause
+#     else
+#         spoticli
+#     fi
+# fi
