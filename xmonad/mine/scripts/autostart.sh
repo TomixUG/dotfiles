aeaa2b8743b86f7246247beda4f7665353c480ae
TomixUG <tomas.sousek1@gmail.com>
2023-12-30 22:55:29 +0100

    change to the asus monitor

--- xmonad/mine/scripts/autostart.sh
+++ xmonad/mine/scripts/autostart.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 #xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1-0 --mode 1280x1024 --pos 1920x56 --rotate normal
 #xrandr --output HDMI-0 --mode 1280x1024 --pos 1920x0 --rotate normal --output eDP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-1 --off
-xrandr --output HDMI-0 --mode 1280x1024 --pos 1920x0 --rotate normal --output eDP-1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-1 --off
+# old monitor:
+# xrandr --output HDMI-0 --mode 1280x1024 --pos 1920x0 --rotate normal --output eDP-1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-1 --off
+
+xrandr --output HDMI-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output eDP-1-1 --mode 1920x1080 --pos 2560x360 --rotate normal --output DP-1-1 --off
+xrandr --output HDMI-0 --mode 2560x1440 --rate 144
 sleep 1
 
 # fix mouse cursor
