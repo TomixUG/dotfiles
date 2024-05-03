037111cc30549a4149b8e9d6c138b739221b90a3
TomixUG <tomas.sousek1@gmail.com>
2024-05-03 16:53:15 +0200

    fix audio output script

--- xmonad/mine/scripts/toggleAudioOut.sh
+++ xmonad/mine/scripts/toggleAudioOut.sh
@@ -12,14 +12,19 @@ if [[ $1 = 'p' ]]; then
 	exit 0
 fi
 
+SINKOUT=$(pactl list sinks | grep "Sink #" | tail -1 | cut --delimiter=" " --fields 2 | cut -c 2-)
+SINKIN=$(pactl list sources | grep "Source #" | tail -1 | cut --delimiter=" " --fields 2 | cut -c 2-)
+
 # change it
 echo "setting microphone to headset"
-pactl set-source-port 53 analog-input-headset-mic
+pactl set-source-port $SINKIN analog-input-headset-mic
 
 if [[ $ACTIVE = 'analog-output-speaker' ]]; then
 	echo "[speaker] setting to headphones..."
-	pactl set-sink-port 52 analog-output-headphones
+	notify-send -r 6322 "setting to headphones..."
+	pactl set-sink-port $SINKOUT analog-output-headphones
 elif [[ $ACTIVE = 'analog-output-headphones' ]]; then
 	echo "[headphones] setting to speaker..."
-	pactl set-sink-port 52 analog-output-speaker
+	notify-send -r 6322 "setting to speaker..."
+	pactl set-sink-port $SINKOUT analog-output-speaker
 fi
